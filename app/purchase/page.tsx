eb915231a8f3f45ed265d9e1e3b0bbb77b82df2f
belbelbelbel <gronaldchia@gmail.com>
2024-09-14 14:13:42 +0100

    styled on logic

--- app/purchase/page.tsx
+++ app/purchase/page.tsx
@@ -13,7 +13,8 @@ export default function PurchasePage() {
         <div className='w-screen h-[100vh] overflow-auto'>
             <div className='fixed px-8 w-full flex z-50 bg-[#f4f4f0] items-center justify-between shadow-lg h-[14vh]'>
                 <div className='cursor-pointer relative'>
-                    <Image src={'/images/gote-logo-removebg.png'} alt='Logo' width={135} height={50} priority={true} sizes={''} objectFit={'cover'} />
+                    {/* <Image src={'/images/gote-logo-removebg.png'} alt='Logo' width={135} height={50} priority={true} sizes={''} objectFit={'cover'} /> */}
+                    <img src="/images/gote-logo-removebg.png" alt="logo"  width={135} height={50} />
                 </div>
                 <div>
                     <NavbarrRoutes />
