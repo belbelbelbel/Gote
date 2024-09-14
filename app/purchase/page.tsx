f98cf3be00315377d60bb834038c4bf578282e87
belbelbelbel <gronaldchia@gmail.com>
2024-09-14 12:08:10 +0100

    styled on logic

--- app/purchase/page.tsx
+++ app/purchase/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useRef } from 'react'
 import { ImageField } from '../Component/ImageField'
+import Image from 'next/image'
 import NavbarrRoutes from '../Auth/NavbarRoutes/NavbarRotes'
 import { FaCartArrowDown } from "react-icons/fa";
 import { motion } from 'framer-motion'; // Import Framer Motion
@@ -12,7 +13,7 @@ export default function PurchasePage() {
         <div className='w-screen h-[100vh] overflow-auto'>
             <div className='fixed px-8 w-full flex z-50 bg-[#f4f4f0] items-center justify-between shadow-lg h-[14vh]'>
                 <div className='cursor-pointer relative'>
-                    <ImageField src={'/images/gote-logo-removebg.png'} alt='Logo' width={135} height={50} priority={true} sizes={''} objectFit={'cover'} />
+                    <Image src={'/images/gote-logo-removebg.png'} alt='Logo' width={135} height={50} priority={true} sizes={''} objectFit={'cover'} />
                 </div>
                 <div>
                     <NavbarrRoutes />
